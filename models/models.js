39e911d149334d5ad43eb74cad5ead291c9fdf79
JesusAlgora <jesus.algora@gmail.com>
2015-07-25 08:07:57 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,51 +1,58 @@
 var path = require('path');
 //Postgres DABABASE_URL = postgres://user:passwd@host:port/database
 //SQLite   DATABASE_URL = sqlite://@:/
+//var url = (process.env.DATABASE_URL || 'sqlite://:@:/').match(/(.*)\:\/\/(.*?)\emoticon.*)@(.*)\emoticon.*)\/(.*)/);
+//var url = (process.env.DATABASE_URL || 'sqlite://:@:/').match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+//var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 
-var url = then.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var protocol   = (url[1]|null);
-var dialect    = (url[1]|null);
-var user       = (url[2]|null);
-var pwd        = (url[3]|null);
-var port       = (url[5]|null);
+//var protocol   = (url[1]|null);
+//var dialect    = (url[1]|null);
+//var user       = (url[2]|null);
+//var pwd        = (url[3]|null);
+/*var port       = (url[5]|null);
 var host       = (url[4]|null);
 var DB_name    = (url[6]|null);
 
-var storage    = then.env.DATABASE_STORAGE;
+var storage    = process.env.DATABASE_STORAGE;*/
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
-var sequelize = new Sequelize (BD_name, user, pwd, 
-				{
-					dialect : protocol, 
-					protocol: protocol,
-					port    : port,
-					host    : host,
-					storage : storage,  // solo SQLite (.env)
-					omiNull : true      // solo Postgres
-				}
-					
+var sequelize = new Sequelize (DB_name, user, pwd, 
+	{dialect : dialect, 
+	 protocol: protocol,
+	 port    : port,
+	 host    : host,
+	 storage : storage,  // solo SQLite (.env)
+	 omitNull: true      // solo Postgres
+	}				
 );
 
 //Importar la definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(_dirname,'quiz'));
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
 exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en BD
-sequelize.sync().then(function() {
-	// then(..) ejecuta el manejador duna vez creada la tabla
+sequelize.sync().then(function(){
+	// then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(count) {
-		If (count === 0) { // la tabla de inicializa solo si esta vacia
-			Quiz.create({ pregunta: 'Capital de Italia',
-						  respuesta: 'Roma
-						  '})
+		if (count === 0) { 
+			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'})
+			Quiz.create({ pregunta: 'Capital de España', respuesta: 'Madrid'})
 			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
-
-
 
